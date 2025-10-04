859c9d48a0b696c24d634445922d7adb9fedf277
Devsi-git <yousefi.devsi@gmail.com>
2025-10-04 21:03:33 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FilmSection from "./components/FilmSection";
 import Header from "./components/Header";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 1500));
 
