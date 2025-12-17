dfbc6612d7f227c9e51655180104c3fd1a02aba0
Devsi-git <yousefi.devsi@gmail.com>
2025-12-17 12:07:43 +0330

    supabase update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { poppins, zenDots } from "@/lib/fonts";
 
 import Aside from "./components/SideBar";
-import SessionKeepAlive from "@/lib/SessionKeepAlive";
 
 export const metadata: Metadata = {
   title: "WAVEFLIX",
@@ -21,7 +20,6 @@ export default function RootLayout({
         className={`bg-[#090909] ${zenDots.variable} ${poppins.variable}  font-poppins text-white`}
       >
         <Aside />
-        <SessionKeepAlive />
         {children}
       </body>
     </html>
--- src/lib/setSession.ts
+++ src/lib/setSession.ts
@@ -3,7 +3,6 @@ import { cookies } from "next/headers";
 import { Session } from "@supabase/supabase-js";
 import { supabase } from "@/supabase/initialize";
 import { redirect } from "next/navigation";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 
 export async function setSession(session: Session) {
   const { access_token, refresh_token } = session;
@@ -17,20 +16,6 @@ export async function setSession(session: Session) {
 
   cookieStore.set("sb-access", access_token, cookieOptions);
   cookieStore.set("sb-refresh", refresh_token, cookieOptions);
-}
-
-export async function refreshSession() {
-  const cookieStore = cookies();
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
-
-  const { data, error } = await supabase.auth.getSession();
-
-  if (error || !data.session) {
-    console.error("Error refreshing session:", error);
-    return;
-  }
-
-  await setSession(data.session);
 }
 
 export async function logout() {
